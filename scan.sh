c5d499485b8b652b646348edf8d6c65d65d3471f
beehead <beehead@gmail.com>
2019-11-27 14:26:03 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,15 +1,16 @@
 #/bin/bash
-# needed packages tar, git, nmap, nmap-scripts, mailutils
+# needed packages tar, git, nmap, mailutils
 # needed list of IP addresses to scan in file scan.ips
 # script runs nmap with vulnerscom script and send results on email. Inspired by Flan scan
+# tested on Debian 9
 # download script. To check repo
 git clone https://github.com/vulnersCom/nmap-vulners /usr/share/nmap/scripts/vulners && nmap --script-updatedb
 
 # run scan. To check keys
-nmap -sV -oX ./xml_files -oN - -v1 $@ --script=vulners/vulners.nse -iL ./scan.ips
+nmap -sV -oX ./xml_files.xml -oN - -v1 $@ --script=vulners/vulners.nse -iL ./scan.ips
 
 # pack results
-tar -czf xml_files.tar.gz xml_files
+tar -czf xml_files.tar.gz xml_files.xml
 
 # send email with results
-echo "See results in attachment" | mail -s "Scan results" -A xml_files.tar.gz user@example.com
+echo "See results in attachment" | mutt -s "Scan results" -a xml_files.tar.gz user@example.com
