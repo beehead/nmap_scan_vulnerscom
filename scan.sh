37cf987589ff080241baa1cfae98719bf54f971f
beehead <beehead@gmail.com>
2019-11-27 15:09:02 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,11 +1,12 @@
 #/bin/bash
-# needed packages tar, git, nmap, mailutils
+# needed packages tar, git, nmap, mailutils, xsltproc
 # needed list of IP addresses to scan in file scan.ips
 # script runs nmap with vulnerscom script and send results on email. Inspired by Flan scan
 # tested on Debian 9
 # define current time and output filename
 current_time=$(date "+%Y.%m.%d-%H.%M")
 filename=xml_files/$current_time.xml
+filename_html=xml_files/$current_time.html
 
 # download script. To check repo
 git clone https://github.com/vulnersCom/nmap-vulners /usr/share/nmap/scripts/vulners && nmap --script-updatedb
@@ -13,8 +14,11 @@ git clone https://github.com/vulnersCom/nmap-vulners /usr/share/nmap/scripts/vul
 # run scan. To check keys
 nmap -Pn -sV -oX $filename -oN - -v1 $@ --script=vulners/vulners.nse -iL ./scan.ips
 
+# convert to html
+xsltproc $filename o $filename_html
+
 # pack results
-tar -czf xml_files.tar.gz $filename
+tar -czf xml_files.tar.gz $filename $filename_html
 
 # send email with results
 echo "See results in attachment" | mutt -s "Scan results" -a xml_files.tar.gz -- user@example.com
