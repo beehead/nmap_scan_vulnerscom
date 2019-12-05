aa607a852857912c0e09b1936c0032db3714c657
beehead <beehead@gmail.com>
2019-12-05 10:22:00 +0300

    run as, -y

--- scan.sh
+++ scan.sh
@@ -1,32 +1,33 @@
 #/bin/bash
 # script runs nmap with vulnerscom script generates and sends xml and html reports on email, inspired by Flan scan
+# run as root
 # needed packages tar, git, nmap, mailutils, xsltproc
 # needed list of IP addresses to scan in file scan.ips
 # tested on Debian 9
 # check and install packages when needed
 if [ $(dpkg-query -W -f='${Status}' tar 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install tar;
+  apt-get --assume-yes install tar;
 fi
 
 if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install git;
+  apt-get --assume-yes install git;
 fi
 
 if [ $(dpkg-query -W -f='${Status}' nmap 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install nmap;
+  apt-get --assume-yes install nmap;
 fi
 
 if [ $(dpkg-query -W -f='${Status}' mailutils 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install mailutils;
+  apt-get --assume-yes install mailutils;
 fi
 
 if [ $(dpkg-query -W -f='${Status}' xsltproc 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install xsltproc;
+  apt-get --assume-yes install xsltproc;
 fi
 
 # define current time and output filename
